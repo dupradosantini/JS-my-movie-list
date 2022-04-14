de66c87cf88ceeb67823283b604453589d140b85
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-04-14 15:15:08 -0300

    Controlling the Backdrop and Cancel button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,30 @@ const addMovieModal = document.getElementById("add-modal");
 
 const startAddMovieButton = document.querySelector('header button');
 
+const backdrop = document.getElementById("backdrop");
+
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+
 
 function toggleMovieModal(){
     addMovieModal.classList.toggle("visible");
+    toggleBackdrop();
+}
+
+function toggleBackdrop(){
+    backdrop.classList.toggle('visible');
+}
+
+function cancelAddMovie(){ //Cancel button functionality
+    toggleMovieModal();
+}
+
+function backdropClickHandler(){ //Cancel by clicking on backdrop
+    toggleMovieModal();
 }
 
 
 
-
-
 startAddMovieButton.addEventListener("click", toggleMovieModal);
-
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
